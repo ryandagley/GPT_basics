794c3f2ba2af7486ec12a715e9002780ea9dbe6e
unknown <rhdagley@gmail.com>
2023-08-16 06:52:54 -0700

    Adds outline for Text Summarizer script

---
+++ text_summarizer.py
@@ -0,0 +1,13 @@
+# This is a script to summarize large chunks of text using GPT
+
+# load API key
+
+# load text data
+
+# pre-process the text data into chunks
+
+# generate summaries of those chunks
+
+# put them all together 
+
+# dump them
